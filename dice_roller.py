d11fa044d19f80e48e4d17d61b488559e0604c72
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-13 22:46:57 +1000

    Refactor dice roll program and added input validation

--- dice_roller.py
+++ dice_roller.py
@@ -8,12 +8,12 @@ import random
 def main():
     """Simulate a die/dice roll"""
     print("Welcome to the dice roll simulator.")
-    print("Enter a negative number to quit.")
-    number_of_die = int(input("Number of die: "))
-    while number_of_die > 0:
+    print("Enter 0 quit.")
+    number_of_die = get_valid_die_amount()
+    while number_of_die != 0:
         simulate_dice_roll(number_of_die)
-        print("Enter a negative number to quit.")
-        number_of_die = int(input("Number of die: "))
+        print("Enter 0 to quit.")
+        number_of_die = get_valid_die_amount()
     print("Farewell")
 
 
@@ -21,6 +21,15 @@ def simulate_dice_roll(number_of_die):
     """Simulate dice roll"""
     for i in range(number_of_die):
         print(random.randint(1, 6))
+
+
+def get_valid_die_amount():
+    number_of_die = (input("Number of die: "))
+    while number_of_die.isnumeric() is False:
+        print("Invalid")
+        number_of_die = (input("Number of die: "))
+    number_of_die = int(number_of_die)
+    return number_of_die
 
 
 main()

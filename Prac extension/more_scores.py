1cd7b7e1e11f55052bdb50f450287f6c320a482d
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-16 20:12:07 +1000

    Add exception to get valid number function in more scores program

--- Prac extension/more_scores.py
+++ Prac extension/more_scores.py
@@ -1,13 +1,17 @@
 """CP1402 PRAC 2 EXTENSION with extra"""
 import random
 
+FILE_NAME = "results.txt"
+
 
 def main():
-    number_of_scores = int(input("Number of scores: "))
+    outfile = open(FILE_NAME, "w")
+    number_of_scores = get_valid_number()
     for i in range(number_of_scores):
         random_number = random.randint(1, 100)
         result = determine_score_category(random_number)
-        print(f"{random_number} is {result}")
+        print(f"{random_number} is {result}", file=outfile)
+    outfile.close()
 
 
 def determine_score_category(score):
@@ -21,6 +25,18 @@ def determine_score_category(score):
     else:
         result = "Excellent"
     return result
+
+
+def get_valid_number():
+    """Get valid number"""
+    is_valid_number = False
+    while not is_valid_number:
+        try:
+            number_of_scores = int(input("Number of scores: "))
+            is_valid_number = True
+        except ValueError:
+            print("Invalid input")
+    return number_of_scores
 
 
 main()

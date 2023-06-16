c5fbbb12ca7ea187e242617ef25f94a5a82612fe
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-16 20:34:38 +1000

    Generate 15 float values to file for temp converter program

--- Prac extension/temp_converter.py
+++ Prac extension/temp_converter.py
@@ -1,11 +1,17 @@
 """CP1402 PRAC 2 EXTENSION with extra"""
 import random
 
+OUT_FILE = "temps_input.txt"  # is a file with 15 random float value between -200 and +200
+
 
 def main():
+    out_file = open(OUT_FILE, "w")
+
     for i in range(15):
         random_temperature_fahrenheit = random.uniform(-200, 200)
-        print(f"{random_temperature_fahrenheit} F -> {convert_fahrenheit_to_celsius(random_temperature_fahrenheit)} C")
+        print(f"{random_temperature_fahrenheit}", file=out_file)
+
+    out_file.close()
 
 
 def convert_fahrenheit_to_celsius(fahrenheit: float):
